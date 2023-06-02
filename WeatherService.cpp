8b7b5cceb78033693d909370ab59ea82dc66b4c1
”belred” <anitakat2004.12@gmail.com>
2023-06-02 16:28:25 +0300

    Добавлен файл weather.xml, проверена работа сервиса по xml

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
 #include <clocale>
+#include "XmlService.h"
 using namespace std;
 
 int main()
 {
 	setlocale(LC_CTYPE, "rus");
-
+	XmlService xs;
+	Weather w_2 = xs.getWeather("weather.xml");
+	w_2.print();
 }
