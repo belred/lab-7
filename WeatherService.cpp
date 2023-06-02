1c49cea435372ac612956973614f77628da4bf2c
”belred” <anitakat2004.12@gmail.com>
2023-06-02 16:19:27 +0300

    добавлен файл weather.json, проверена работа сервиса по json

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
 #include <clocale>
+#include "JsonService.h"
 using namespace std;
 
 int main()
 {
 	setlocale(LC_CTYPE, "rus");
-
+	JsonService js;
+	Weather w_1 = js.getWeather("weather.json");
+	w_1.print();
 }
